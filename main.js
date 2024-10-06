0ecdd30c1f6bbd3a7f3c202710284c510027f4e2
Ladislau-18 <ladislaucabanga17@gmail.com>
2024-10-07 00:50:10 +0100

    tornando responsivo

--- main.js
+++ main.js
@@ -5,10 +5,7 @@ let data = document.querySelector("#data_tarefa");
 let hora = document.querySelector("#hora_tarefa");
 
 
-let lista_de_tarefa = [
-    { tarefa: "Ladislau António Lote Cabanga ", concluida: false, data: 10_12_2005, hora: 17_30},
-    { tarefa: "Cabanga", concluida: false, data: 2005, hora: 17-30}
-];
+let lista_de_tarefa = [1];
 
 function add_tarefa() {
     if (input.value.trim() !== "" && data.value.trim() !== "" &&  hora.value.trim() !== "") {
@@ -37,17 +34,18 @@ function mostrar_lista() {
             li += `
                 <li id="${item.concluida &&  "concluido"}">
                 		<div id="left">
-                			
-	                    	<img src="icon_check.png" alt="" class="icon" id="feito" onclick="finalizar_tarefa(${indice})">
+                		
+	                    	
+	                    	<svg class="icon" id="feito" onclick="finalizar_tarefa(${indice})" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48"><defs><mask id="ipTCheckOne0"><g fill="none" stroke="#fff" stroke-linejoin="round" stroke-width="4"><path fill="#555" d="M24 44a19.94 19.94 0 0 0 14.142-5.858A19.94 19.94 0 0 0 44 24a19.94 19.94 0 0 0-5.858-14.142A19.94 19.94 0 0 0 24 4A19.94 19.94 0 0 0 9.858 9.858A19.94 19.94 0 0 0 4 24a19.94 19.94 0 0 0 5.858 14.142A19.94 19.94 0 0 0 24 44Z"/><path stroke-linecap="round" d="m16 24l6 6l12-12"/></g></mask></defs><path fill="#ff6600" d="M0 0h48v48H0z" mask="url(#ipTCheckOne0)"/></svg>
                     	
                     	<span id="span_tarefa">${item.tarefa}</span>
                     </div>
                     
                     <div id="right">
-                    	<span>dia ${item.data}</span>
+                    	<span>${item.data}</span>
               	     <span>, ás ${item.hora}</span>
-                    
-                  	  <img src="icon_balde_lixo.png" alt="" class="icon" id="apagar" onclick="apagar_tarefa(${indice})">
+                  	  
+                  	  <svg class="icon" id="apagar" onclick="apagar_tarefa(${indice})" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48"><defs><mask id="ipTDelete0"><g fill="none" stroke="#fff" stroke-linejoin="round" stroke-width="4"><path fill="#555" d="M9 10v34h30V10z"/><path stroke-linecap="round" d="M20 20v13m8-13v13M4 10h40"/><path fill="#555" d="m16 10l3.289-6h9.488L32 10z"/></g></mask></defs><path fill="#ff6600" d="M0 0h48v48H0z" mask="url(#ipTDelete0)"/></svg>
                   	</div>
                 </li>
             `;
@@ -56,16 +54,27 @@ function mostrar_lista() {
     }
 }
 
+
+let confirmar = document.querySelector ("#confirmar")
+
+
 function apagar_tarefa(indice) {
-    let confirmar = confirm("Deseja apagar esta tarefa?");
+
+		document.body.classList.add('desabled')
+		
+    confirmar.classList.toggle('mostrar')
+      let ok = document.querySelector("#Ok").addEventListener("click", function () {
+      			lista_de_tarefa.splice(indice, 1);
+      			
+      			confirmar. classList.remove('mostrar');
+						mostrar_lista();
+						document.body.classList.remove('desabled')
+      })
       
-      if (confirmar) {
-      	lista_de_tarefa.splice(indice, 1);
-				mostrar_lista();
-      }
-    	
+      
     
 }
+
 
 function finalizar_tarefa(indice) {
     lista_de_tarefa[indice].concluida = !lista_de_tarefa[indice].concluida;
@@ -75,4 +84,4 @@ function finalizar_tarefa(indice) {
 botao.addEventListener('click', add_tarefa);
 mostrar_lista();
 
-
+ 
