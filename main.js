5a8f0529cdb5b19ac305bba04589ffa74f0c9e5e
ladislau-18 <ladislaucabanga17@gmail.com>
2025-03-26 09:02:35 +0000

    finished

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 
 let botao = document.querySelector("#adicionar");
+let bt_cancel = document.querySelector("#cancelar_add")
 let input = document.querySelector("#texto_tarefa");
 let toda_L = document.querySelector("#container_2");
 let data = document.querySelector("#data_tarefa");
 let hora = document.querySelector("#hora_tarefa");
-
+let bt_add = document.querySelector("#icon_add_list")
+let menu_add = document.querySelector("#add_tarefa");
 
 let lista_de_tarefa = []; 
 
@@ -20,16 +22,23 @@ function add_tarefa() {
         data.value = "";
         hora.value = "";
         mostrar_lista();
+        menu_add.style.display = "none"
     }
     else {
     	alert("ALERT: Preencha todos os campos")
     }
 }
 
+cancelar = () =>	menu_add.style.display = "none";
+	
+
+
+
+
 function mostrar_lista() {
     let li = "";
     if (lista_de_tarefa.length == 0) {
-        toda_L.innerHTML = `<span id="tarefa_vazia">Nenhuma tarefa adicionada</span>`;
+        toda_L.innerHTML = `<span id="tarefa_vazia">No task added</span>`;
     } else {
         lista_de_tarefa.forEach((item, indice) => {
             li += `
@@ -40,10 +49,9 @@ function mostrar_lista() {
                     	<span id="span_tarefa">${item.tarefa}</span>
                     </div>
                     
-                    <div id="right">
-                    	<span>${item.data}</span>
-              	     <span>, ás ${item.hora}</span>
-                  	  
+                    <div id="right" >
+                    	<span>${item.data} , ás ${item.hora}</span>
+              	     
                   	  <svg class="icon" id="apagar" onclick="apagar_tarefa(${indice})" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48"><defs><mask id="ipTDelete0"><g fill="none" stroke="#fff" stroke-linejoin="round" stroke-width="4"><path fill="#555" d="M9 10v34h30V10z"/><path stroke-linecap="round" d="M20 20v13m8-13v13M4 10h40"/><path fill="#555" d="m16 10l3.289-6h9.488L32 10z"/></g></mask></defs><path fill="#ff6600" d="M0 0h48v48H0z" mask="url(#ipTDelete0)"/></svg>
                   	</div>
                 </div>
@@ -52,12 +60,17 @@ function mostrar_lista() {
         toda_L.innerHTML = li;
     }
 }
-
+document.querySelector("#icon_add_list").addEventListener('click', ()=> {
+    menu_add.style.display = "block"
+    document.querySelector("#containerMain")
+    
+    
+} );
 
 
 function apagar_tarefa(indice) {
     let al = confirm("Deseja apagar esta tarefa?")
-    let ali = al == true? lista_de_tarefa.splice(indice, 1) && confirm("Tarefa apagada com sucesso") : "";
+    let ali = al == true? lista_de_tarefa.splice(indice, 1) : "";
     mostrar_lista();
 
 }
@@ -70,6 +83,7 @@ function finalizar_tarefa(indice) {
 }
 
 botao.addEventListener('click', add_tarefa);
+cancelar_add.addEventListener('click', cancelar);
 mostrar_lista();
 
  
